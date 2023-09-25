8607742d897cdfe4d5755d8e293317783fcaa96d
Nahue <nahue.santini@gmail.com>
2023-09-25 08:48:35 -0300

    Creacion carpetas Components/Home y archivos home.jsx con su modulo de estilo

---
+++ FrontEnd/src/Components/Home/Home.jsx
@@ -0,0 +1,11 @@
+import style from "./home.module.css";
+
+export const home = () => {
+  return (
+    <>
+      <div className={style.title}>
+        <h1> HOME </h1>
+      </div>
+    </>
+  );
+};
