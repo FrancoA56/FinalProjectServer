de3f0a7afe98906072dbeb807a9079de1c8d2ba6
zTom1909 <topolococo2@gmail.com>
2023-09-25 15:22:59 -0300

    feat(src/controllers src/handlers): Added handler and improved base controller to shop route

--- BackEnd/src/controllers/shop/addShopController.ts
+++ BackEnd/src/controllers/shop/addShopController.ts
@@ -1,8 +1,25 @@
 import { Request, Response } from "express";
+import addShopHandler from "../../handlers/shop/addShopHandler";
 
 const addShopController = async (
   req: Request,
   res: Response
-): Promise<void> => {};
+): Promise<void> => {
+  try {
+    interface User {
+      email?: string;
+    }
+    interface Product {
+      id?: number;
+      quantity?: number;
+    }
+    const { products }: { products: Product[] } = req.body;
+    const { email }: User = req.body;
+    const product = await addShopHandler(email, products);
+    res.status(201).json(product);
+  } catch (error) {
+    res.status(500).json({ error: error.message });
+  }
+};
 
 export default addShopController;
