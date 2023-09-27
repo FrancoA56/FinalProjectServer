e6c5ecf8a71e909d9984a889bf4795587131d2bb
AMMatia <adrianm.m-@hotmail.com>
2023-09-26 22:04:54 -0300

    feature(handlers/registerHandler.ts): added functionality

--- src/handlers/user/registerHandler.ts
+++ src/handlers/user/registerHandler.ts
@@ -1,9 +1,24 @@
-const registerHandler = (
-    email: string | undefined,
-    password: string | undefined,
-    name: string | undefined,
-    logo: string | undefined
-  ) => {};
-  
-  export default registerHandler;
-  
+const { User } = require('../../db');
+
+const registerHandler = async (
+  email: string | undefined,
+  password: string | undefined,
+  name: string | undefined,
+  logo: string | undefined
+) => {
+  if (!email || !password || !name) {
+    throw new Error('Faltan datos');
+  }
+  const existingUser = await User.findOne({ where: { email } });
+  if (existingUser) {
+    throw new Error('El usuario ya existe');
+  }
+  const newUser = await User.create({
+    email,
+    password,
+    name,
+    logo,
+  });
+  return newUser;
+};
+
