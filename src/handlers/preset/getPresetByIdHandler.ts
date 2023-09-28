50ba6f42d5fbebef331d36f4b0add2a8aee454af
zTom1909 <topolococo2@gmail.com>
2023-09-28 19:54:28 -0300

    feat(getPresetByIdHandler): Added functionality to get a preset by id

--- src/handlers/preset/getPresetByIdHandler.ts
+++ src/handlers/preset/getPresetByIdHandler.ts
@@ -1,3 +1,11 @@
-const getPresetByIdHandler = async (id: number | undefined) => {};
+import { Preset } from "../../db";
+const getPresetByIdHandler = async (id: number | undefined) => {
+  if (!id) throw new Error("Missing id");
+
+  const preset = await Preset.findOne({ where: { id } });
+  if (!preset) throw new Error("Preset not found with the specified id");
+
+  return preset;
+};
 
 export default getPresetByIdHandler;
