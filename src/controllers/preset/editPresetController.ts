89a383dc7ea52ed8313392557942d4016b420141
zTom1909 <topolococo2@gmail.com>
2023-09-29 19:24:28 -0300

    feat(editPreset): Added functionality to edit a preset

--- src/controllers/preset/editPresetController.ts
+++ src/controllers/preset/editPresetController.ts
@@ -1,6 +1,19 @@
 import { Request, Response } from "express";
 import editPresetHandler from "../../handlers/preset/editPresetHandler";
 
+enum PresetTypes {
+  ABOUT = "about",
+  HOME = "home",
+  FORM = "form",
+  CARD = "card",
+}
+
+enum PresetCategories {
+  BASIC = "basic",
+  MEDIUM = "medium",
+  PREMIUM = "premium",
+}
+
 const editPresetController = async (
   req: Request,
   res: Response
@@ -9,11 +22,23 @@ const editPresetController = async (
     interface Preset {
       id?: number;
       name?: string;
-      type?: string;
+      price?: number;
+      color?: string;
+      type?: PresetTypes;
+      category?: PresetCategories;
+      isDisabled?: boolean;
     }
     const { id }: Preset = req.params;
-    const { name, type }: Preset = req.query;
-    const preset = await editPresetHandler(id, name, type);
+    const { name, price, color, type, category, isDisabled }: Preset =
+      req.query;
+    const preset = await editPresetHandler(id, {
+      name,
+      price,
+      defaultColor: color,
+      type,
+      category,
+      isDisabled,
+    });
     res.status(200).json(preset);
   } catch (error) {
     res.status(500).json({ error: (error as Error).message });
