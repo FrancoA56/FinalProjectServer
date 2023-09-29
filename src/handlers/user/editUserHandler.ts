9e15d950749920e397cc547d5b176443b1ab1591
AMMatia <adrianm.m-@hotmail.com>
2023-09-28 21:15:15 -0300

    feature(handlers/editUserHandler): added functionality

--- src/handlers/user/editUserHandler.ts
+++ src/handlers/user/editUserHandler.ts
@@ -1,8 +1,27 @@
-const editUserHandler = (
+import { User } from "../../db";
+const bcrypt = require("bcrypt");
+
+const editUserHandler = async (
   email: string | undefined,
   password: string | undefined,
   name: string | undefined,
   logo: string | undefined
-) => {};
+) => {
+  const user = await User.findOne({ where: { email } });
+  const SALT_ROUNDS: number = 10;
+
+  if (!user) throw new Error("User doesn't exist.");
+
+  if (password) {
+    const hashedPass = await bcrypt.hash(password, SALT_ROUNDS);
+
+    user.dataValues.password = hashedPass;
+  }
+
+  if (name) user.dataValues.name = name;
+  if (logo) user.dataValues.logo = logo;
+
+  return user;
+};
 
 export default editUserHandler;
