fb9a190ccd7e94c57956684ed3319ec0b8d120e8
zTom1909 <topolococo2@gmail.com>
2023-09-29 19:00:19 -0300

    feat(disablePresetHandler): Added functionality to disable a preset

--- src/handlers/preset/disablePresetHandler.ts
+++ src/handlers/preset/disablePresetHandler.ts
@@ -1,3 +1,7 @@
-const disablePresetHandler = async (id: number | undefined) => {};
+import { Preset } from "../../db";
+const disablePresetHandler = async (id: number | undefined) => {
+  await Preset.update({ isDisabled: true }, { where: { id } });
+  return await Preset.findOne({ where: { id } });
+};
 
 export default disablePresetHandler;
