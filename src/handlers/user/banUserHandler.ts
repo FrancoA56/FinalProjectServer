5196ce92f29cc2b939154281a4f34417d1951e7f
AMMatia <adrianm.m-@hotmail.com>
2023-09-28 21:49:44 -0300

    feature(banUser): added functionality and column

--- src/handlers/user/banUserHandler.ts
+++ src/handlers/user/banUserHandler.ts
@@ -1,3 +1,17 @@
-const banUserHandler = (email: string | undefined) => {};
+import { User } from "../../db";
+
+const banUserHandler = async (email: string | undefined) => {
+  const user = await User.findOne({ where: { email } });
+
+  if (!user) throw new Error("User doesn't exist.");
+
+  if (user.dataValues.isDisabled) return "User is already banned";
+
+  user.dataValues.isDisabled = true;
+
+  user.save();
+
+  return user;
+};
 
 export default banUserHandler;
