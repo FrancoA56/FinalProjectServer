be7b0dee5db90ed4dfd25ff8155d70114dc5b410
Rafael Prieto <ing.rafaelprieto@gmail.com>
2023-10-04 11:03:40 -0500

    deploy configuration

--- index.ts
+++ index.ts
@@ -1,13 +1,19 @@
 import { sequelize } from "./src/db";
 import server from "./src/server";
+
+/*Importing this library is necessary 
+for TypeScript decorators to work correctly 
+and prevent deployment errors.*/
+import 'reflect-metadata';
+
 const PORT: number = 3001;
 
 sequelize
-  .sync({ force: true })
+  .sync({ force: false })
   .then(() => {
     console.log("Database synced correctly");
     server.listen(PORT, () => console.log("Server listening on port " + PORT));
   })
-  .catch((error:Error) => {
+  .catch((error:Error) => { 
     console.error(error);
   });
--- src/db.ts
+++ src/db.ts
@@ -1,21 +1,37 @@
 import { Sequelize } from "sequelize-typescript";
 import config from "./utils/config";
-const sequelize = new Sequelize({
-  dialect: "postgres",
-  database: config.dbName,
-  password: config.dbPassword,
-  username: config.dbUser,
-  storage: ":memory:",
-  models: [__dirname + "/models"],
-  logging: false,
-});
+
+let sequelize: Sequelize;
+if (config.dev) {//LOCAL CONNECTION
+    sequelize = new Sequelize({
+    dialect: "postgres",
+    database: config.dbName,
+    password: config.dbPassword,
+    username: config.dbUser,
+    storage: ":memory:",
+    models: [__dirname + "/models"],
+    logging: false,
+  });
+} else {//RENDER CONNECTION
+    sequelize = new Sequelize(config.dbDeploy, {
+    logging: false, // set to console.log to see the raw SQL queries
+    native: false, // lets Sequelize know we can use pg-native for ~30% more speed
+    dialectOptions: {
+      ssl: {
+        require: true,
+      },
+    },
+    models: [__dirname + "/models"],
+  });
+}
+
 const {
   User,
   Admin,
   Preset,
-  ShoppingList,
-  shoppingListItems,
-  reviews,
+  Invoice,
+  InvoiceDetail,
+  Review,
   UserPreset,
 } = sequelize.models;
 
@@ -24,8 +40,8 @@ export {
   User,
   Admin,
   Preset,
-  ShoppingList,
-  shoppingListItems,
-  reviews,
+  Invoice,
+  InvoiceDetail,
+  Review,
   UserPreset,
 };
