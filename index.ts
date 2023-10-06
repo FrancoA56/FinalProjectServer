d61feee01be95836baa4a6200e832387f2ef6cb8
Rafael Prieto <ing.rafaelprieto@gmail.com>
2023-10-06 02:12:13 -0500

    force true in index

--- index.ts
+++ index.ts
@@ -1,16 +1,15 @@
 import { sequelize } from "./src/db";
 import server from "./src/server";
 import 'reflect-metadata';
-import config from "./src/utils/config";
 
-  const PORT: number = 3001;
+const PORT: number = 3001;
 
-  sequelize
-    .sync({ force: config.dev ? true : false })
-    .then(() => {
-      console.log("Database synced correctly");
-      server.listen(PORT, () => console.log("Server listening on port " + PORT));
-    })
-    .catch((error: Error) => {
-      console.error(error);
-    });
+sequelize
+  .sync({ force: true })
+  .then(() => {
+    console.log("Database synced correctly");
+    server.listen(PORT, () => console.log("Server listening on port " + PORT));
+  })
+  .catch((error: Error) => {
+    console.error(error);
+  });
